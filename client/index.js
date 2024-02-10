c6ddf1aeb0d04f3ed9e076591185cc2b0577a581
activiteOCR <julien.terrier3@wanadoo.fr>
2024-02-10 17:43:23 +0100

    Alchemy Ethereum bootcamp week2

--- client/index.js
+++ client/index.js
@@ -5,10 +5,28 @@ const MerkleTree = require('../utils/MerkleTree');
 const serverUrl = 'http://localhost:1225';
 
 async function main() {
-  // TODO: how do we prove to the server we're on the nice list? 
+  // TODO: how do we prove to the server we're on the nice list?
+  // Check if a name is provided as a command line argument
+  const name = process.argv[2]; // Assuming the name is provided as the third argument
+  if (!name) {
+    console.error('Please provide a name as a command line argument.');
+    return;
+  }
+  // create the merkle tree for the whole nice list
+  const merkleTree = new MerkleTree(niceList);
+
+  // get the root
+  const root = merkleTree.getRoot();
+
+  // find the proof that argv[2] is in the list 
+  const index = niceList.findIndex(n => n === name);
+  const proof = merkleTree.getProof(index);
 
   const { data: gift } = await axios.post(`${serverUrl}/gift`, {
-    // TODO: add request body parameters here!
+    name,
+    index,
+    proof,
+    root
   });
 
   console.log({ gift });
--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const verifyProof = require('../utils/verifyProof');
+const niceList = require('../utils/niceList.json');
+const MerkleTree = require('../utils/MerkleTree');
 
 const port = 1225;
 
@@ -8,14 +10,18 @@ app.use(express.json());
 
 // TODO: hardcode a merkle root here representing the whole nice list
 // paste the hex string in here, without the 0x prefix
-const MERKLE_ROOT = '';
+//const merkleTree = new MerkleTree(niceList);
+//const hexString = merkleTree.getRoot();
+
+const MERKLE_ROOT = '6ecddf47bff1a5c9e9ac8b5bd24dd54a1151d223073513305d008edafad30658';
 
 app.post('/gift', (req, res) => {
   // grab the parameters from the front-end here
   const body = req.body;
 
   // TODO: prove that a name is in the list 
-  const isInTheList = false;
+  
+  const isInTheList = verifyProof(body.proof, body.name, MERKLE_ROOT);
   if(isInTheList) {
     res.send("You got a toy robot!");
   }
